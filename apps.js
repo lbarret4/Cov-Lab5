48b8b25e8e218eadc416cce066b326d3e67b9906
lbarret4 <llewellynbar4@gmail.com>
2018-08-18 15:37:58 -0400

    Updated queueNames function to make sure first letter is upper case.

--- apps.js
+++ apps.js
@@ -1,11 +1,12 @@
 let myName = "Llewellyn Barrett";
 const NUMBER_OF_STATES = 50;
-let num = 5+4;
+let num = 5 + 4;
 
-function queueNames(name){
-    if(name.charCodeAt(0) >76){
-         console.log('Back to the line!');
-    }else {
+function queueNames(name) {
+    name = name.toUpperCase();
+    if (name.charCodeAt(0) > 76) {
+        console.log('Back to the line!');
+    } else {
         console.log('Next!')
     }
 }
