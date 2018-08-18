878ba3c6c2203600d405614627f231b25dca78ba
lbarret4 <llewellynbar4@gmail.com>
2018-08-18 16:00:07 -0400

    Added validation of name for queueNames function

--- apps.js
+++ apps.js
@@ -1,12 +1,15 @@
 let myName = "Llewellyn Barrett";
 const NUMBER_OF_STATES = 50;
-let num = 5 + 4;
+let num = 5+4;
 
 function queueNames(name) {
-    name = name.toUpperCase();
-    if (name.charCodeAt(0) > 76) {
+    name = name.toLowerCase();
+    firstLetter=name.charCodeAt(0);
+    if ( firstLetter > 108 && firstLetter < 123) {
         console.log('Back to the line!');
+    } else if(firstLetter > 122 || firstLetter < 97){
+        console.log('Invalid Name');
     } else {
         console.log('Next!')
-    }
+    } 
 }
